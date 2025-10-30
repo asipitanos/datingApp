fb0b1b1d9f6c9be512ef35b529869141586baf20
asipitanos <antonios.sipitanos@gmail.com>
2025-10-30 11:59:03 +0000

    remove unused imports

--- datingApp/app/src/main/java/com/example/datingapp/pages/LandingPage.kt
+++ datingApp/app/src/main/java/com/example/datingapp/pages/LandingPage.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
